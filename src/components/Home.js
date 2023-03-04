ea4e08b35db6538341b25c66f9d67b4cffdd47ee
luancortez <luancortezdev@gmail.com>
2023-03-03 21:57:27 -0300

    feat: :sparkles: Componentes Home e Movies criados

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import Ticket from './../images/movie_tickets.jpg'
+
+export const Home = () => {
+  return (
+    <>
+      <div className="text-center">
+        <h2>Find a movie to watch tonight!</h2>
+        <hr />
+        <img src={Ticket} alt="movie tickets" />
+      </div>
+    </>
+  )
+}
---
+++ src/components/Movies.js
@@ -0,0 +1,10 @@
+export const Movies = () => {
+  return (
+    <>
+      <div className="text-center">
+        <h2>Movies</h2>
+        <hr />
+      </div>
+    </>
+  )
+}
