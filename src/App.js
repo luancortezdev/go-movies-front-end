46ba2585882542122ea23ad218db2c5b1fc44af0
luancortez <luancortezdev@gmail.com>
2023-03-04 19:19:15 -0300

    feat: :sparkles: Componente App implementado
    jwtToken state criado para simular um token jwt. Estados alertMessage e alertClassname criados e passados pelo context do Outlet para ser utilizado no componente Login.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Alert } from "./components/Alert";
 
 function App() {
+  const [jwtToken, setJwtToken] = useState("")
+  const [alertMessage, setAlertMessage] = useState("")
+  const [alertClassName, setAlertClassName] = useState("d-none")
+
+  const navigate = useNavigate()
+
+  const logOut = () => {
+    setJwtToken("")
+    navigate("/login")
+  }
+
   return (
     <div className="container">
       <div className="row">
@@ -8,9 +21,19 @@ function App() {
           <h1>Go Watch a Movie!</h1>
         </div>
         <div className="col text-end">
-          <Link to="/login"><span className="badge bg-success">Login</span></Link>
+          {
+            jwtToken === ""
+              ?
+              <Link to="/login">
+                <span className="badge bg-success">Login</span>
+              </Link>
+              :
+              <a href="#!" onClick={logOut}>
+                <span className="badge bg-danger">Logout</span>
+              </a>
+          }
         </div>
-        <hr className="mb-3"/>
+        <hr className="mb-3" />
       </div>
 
       <div className="row">
@@ -20,17 +43,32 @@ function App() {
               <Link to="/" className="list-group-item list-group-item-action">Home</Link>
               <Link to="/movies" className="list-group-item list-group-item-action">Movies</Link>
               <Link to="/genres" className="list-group-item list-group-item-action">Genres</Link>
-              <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
-              <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalogue</Link>
-              <Link to="/graphql" className="list-group-item list-group-item-action">GrapgQL</Link>
+              {
+                jwtToken !== "" &&
+                <>
+                  <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
+                  <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalogue</Link>
+                  <Link to="/graphql" className="list-group-item list-group-item-action">GrapgQL</Link>
+                </>
+              }
             </div>
           </nav>
         </div>
         <div className="col-md-10">
-          <Outlet />
+          <Alert
+            message={alertMessage}
+            className={alertClassName}
+          />
+          <Outlet
+            context={{
+              setJwtToken,
+              setAlertMessage,
+              setAlertClassName
+            }}
+          />
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
 
